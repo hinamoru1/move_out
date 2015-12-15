807b31fc40b377800eddb2a1803ebd6511ca02d3
Chloedhr <cdreher@hotmail.fr>
2015-12-15 11:46:33 +0100

    Changement de nom pour le fichier confidentialité
    Mon nordi ne voulait pas de "é" dans le titre

--- APP_ PHP_Base/footer.php
+++ APP_ PHP_Base/footer.php
@@ -11,7 +11,7 @@
                 <ul class="lien_footer">
                     <a href="Qui_sommes_nous.html"><h3>Qui sommes-nous?</h3></a></li><!--
                 --><a href="Forum.html"><h3>Forum</h3></a></li><!--
-                --><a href="confidentialité.php"><h3>Confidentialité</h3></a></li><!--
+                --><a href="confidentialite.php"><h3>Confidentialité</h3></a></li><!--
                 --></ul>
             </div>
             <h3 class="nom_site_footer">Move-out.com ®</h3>
